2dca7a03675927b4f17bf5b076ae50aaed079738
Sujay Mohanty <42656214+Sujay-Mohanty@users.noreply.github.com>
2025-04-23 10:28:01 +0000

    Update test.java
    Raman Update

--- test.java
+++ test.java
@@ -2,15 +2,3 @@ class test{
   public void testMethod(){
     assert(false);
   }
-
-<<<<<<< HEAD
-
-=======
-public void testMethod2(){
-assert(true);
-}
-
-  public void testMethod3(){
-  }
->>>>>>> 97d0a6c0cd85d2b5eac97bbff16edfe286208492
-}
